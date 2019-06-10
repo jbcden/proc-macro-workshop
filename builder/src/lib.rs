06126e39366b85fd85d49a3661afbf491eb1173e
Jacob Chae <jbcden@gmail.com>
2019-06-10 14:17:49 +0900

    Make first 2 builder tests pass
    Only the minimum has been done for test 2. I will update with the rest
    in the next commit.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,20 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::{quote};
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let buildee_ident = &ast.ident;
 
-    unimplemented!()
+    let expanded = quote! {
+        impl #buildee_ident {
+            pub fn builder() {}
+        }
+    };
+
+
+    expanded.into()
 }
