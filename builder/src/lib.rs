17c4b6f0968646e0288457e1953bab9b742e14ee
Jacob Chae <jbcden@gmail.com>
2019-06-10 14:25:41 +0900

    Add dynamically generated builder struct
    The next thing to do is make the entire builder struct be dynamically
    generated based on the content of the derived struct.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,10 +8,26 @@ use syn::{parse_macro_input, DeriveInput, Ident};
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let buildee_ident = &ast.ident;
+    let builder_name = format!("{}Builder", buildee_ident);
+    let builder_ident = Ident::new(&builder_name, ast.ident.span());
 
     let expanded = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
         impl #buildee_ident {
-            pub fn builder() {}
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
         }
     };
 
